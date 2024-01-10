ec6dbb2ab42155621996cee7a71449b99cf6e725
MSChella <reachchellapathy.ms@gmail.com>
2024-01-10 18:47:57 +0530

    Updated the todo app with complete business logic. Just logic and work to do on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
-import ToDoTask from './components/CreateToDo/ToDoTask';
 import ToDoCard from './components/ToDoCard/ToDoCard';
+import TodoApp from './components/ToDo/ToDoApp';
 
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState({ name: '', description: '' })
+
+  const addToDo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo({ name: '', description: '' })
+
+  };
+
+
   return (
     <div className="App">
-      <ToDoTask />
-      <ToDoCard />
+      <div className='container'>
+        <div className='row rowgap'>
+          <div className='col'>
+            <input type='text' value={newTodo.name} onChange={(e) => setNewTodo({ ...newTodo, name: e.target.value })}></input>
+          </div>
+          <div className='col'>
+            <input type='text' value={newTodo.description} onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}></input>
+          </div>
+          <div className='col'>
+            <button type='text' onClick={addToDo}>Add Todos</button>
+          </div>
+        </div>
+      </div>
+      <div className='grid'>
+        {todos.map((newTodo, id) =>
+
+          <ToDoCard value={newTodo} key={id} />)
+
+        }
+
+      </div>
+
     </div>
+
   );
 }
 
--- src/components/ToDoCard/ToDoCard.jsx
+++ src/components/ToDoCard/ToDoCard.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
-const ToDoCard = () => {
+const ToDoCard = ({ value }) => {
 
     return (
-        <div className='container'>
-            <div className='row cols-2 row cols-md-3 row cols-xl-4 justify-content-center'>ToDoCard </div>
+
+        <div className='cols-md-3 row cols-xl-4 justify-content-center'>
+            <div className='task_box'>
+                <p>Name:{value.name}</p>
+                <p>Description:{value.description}</p>
+                <div className='row'>
+                    <p>Status:</p>
+
+                </div>
+
+
+
+            </div>
 
 
         </div>
+
+
+
     )
 
 }
